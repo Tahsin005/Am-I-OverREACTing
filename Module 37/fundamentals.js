8924454c0b351388fb910a4275a9f0a7af086571
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-06-21 17:32:11 +0600

    module 37 introuduction

---
+++ Module 37/fundamentals.js
@@ -0,0 +1,37 @@
+// How to declare variables using let and const
+const name = 'Tahsin';
+let value = 10;
+value = 9;
+
+// Condition 
+if (4) {
+    
+} else if (1) {
+
+} else {
+    
+}
+
+// Array 
+let myArray = [];
+
+// Loop 
+array.forEach(element => {
+    
+});
+
+// Function
+
+function myFunction() {
+    
+}
+
+// Object
+
+const myObject = {
+    name: 'Tahsin',
+    age: 20
+}
+
+console.log(Object.name);
+console.log(Object['name']);
