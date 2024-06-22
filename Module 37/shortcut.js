ba4414983f81ac4bc988e1729eeac7f16df89221
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-06-22 19:42:59 +0600

    Truthy, Falsy, Ternary Operator, Shortcut And Or

---
+++ Module 37/shortcut.js
@@ -0,0 +1,29 @@
+// 'tahsin', 5, true, {}, []
+// false, '', null, 0, undefined
+
+let value = 10;
+if (value) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+
+// ternary operator
+const money = 80;
+let food = money > 100? 'Biriyani': 'cha biscuits';
+
+console.log(food);
+
+// number to string 
+const num1 = 52;
+console. log(numl) ;
+const numStr = numl + '';
+console. log(numStr);
+
+// string to number
+const input = '523';
+const inputNum = +input;
+console.log(inputNum);
+
+let isActive = false;
+isActive = !isActive;
