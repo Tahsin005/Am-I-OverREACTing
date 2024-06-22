c3a0b3d37671041aeba78625fd1b6a3ef9df15c9
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-06-22 23:31:26 +0600

    Dot Vs Bracket Notation

---
+++ Module 37/notation.js
@@ -0,0 +1,14 @@
+const person = {
+    name: 'Tahsin',
+    age: 20,
+    profession : 'admin',
+    address: '123 Main Street'
+}
+
+// dot notation
+console.log(person.name);
+
+
+// bracket notation
+// if the key is not a string, then i have to use this bracket notation
+console.log(person['name']);
