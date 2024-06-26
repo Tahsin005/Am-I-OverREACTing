2cbb18a50787eeebf8dd9f9c46286a32485bb1e2
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-06-26 11:32:13 +0600

    my first react component

--- Module 38/explore-react-core-concepts/src/App.jsx
+++ Module 38/explore-react-core-concepts/src/App.jsx
@@ -9,8 +9,28 @@ function App() {
     <>
       
       <h1>Vite + React</h1>
+      <Person></Person>
+      <Student></Student>
     </>
   )
 }
 
+function Person () {
+  const age = 22;
+  const person = {
+    name: 'sakib',
+    age: 12
+  }
+  return <h3>I am a {person.name} with age {age}</h3>
+}
+
+function Student () {
+  return (
+    <div>
+      <h3>I am a student</h3>
+      <p>Name: Tahsin</p>
+      <p>Age: 12</p>
+    </div>
+  )
+}
 export default App
