a97a8acc6e29f509beba88dc42d54ef313840e37
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-06-26 17:50:38 +0600

    Conditional Rendering: If, Ternary And AND OR

--- Module 38/explore-react-core-concepts/src/Todo.jsx
+++ Module 38/explore-react-core-concepts/src/Todo.jsx
@@ -1,6 +1,34 @@
+// export default function Todo ({task, isDone}) {
+//     if (isDone) {
+//         return <li>{task}</li>
+//     }
+//     return <li>{task} kore nai</li>
+// }
+
+// conditional rendering (ternary operator)
+// export default function Todo ({task, isDone}) {
+//     return (
+//         <li>{task} {isDone? 'korse' : 'kore nai'}</li>
+//     )
+// }
+
+// export default function Todo ({task, isDone}) {
+//     return (
+//         <li>{task} {isDone && ': is done'}</li>
+//     )
+// }
+
+// export default function Todo ({task, isDone}) {
+//     return (
+//         <li>{task} {isDone || ': do it'}</li>
+//     )
+// }
+
 export default function Todo ({task, isDone}) {
+    let listItem;
     if (isDone) {
-        return <li>{task}</li>
-    }
-    return <li>{task} kore nai</li>
+        listItem = <li>{task} Korse</li>
+    } else listItem = <li>{task} kore nai</li>
+
+    return listItem;
 }
