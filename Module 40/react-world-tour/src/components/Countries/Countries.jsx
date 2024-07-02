4b5f7d5a1909283e82bb153552a2990d8a16f89a
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-07-02 20:39:21 +0600

    Conditional CSS And Conditional Style

--- Module 40/react-world-tour/src/components/Countries/Countries.jsx
+++ Module 40/react-world-tour/src/components/Countries/Countries.jsx
@@ -3,20 +3,36 @@ import Country from "../Country/Country";
 import './Countries.css'
 const Countries = () => {
     const [countries, setCountries] = useState([]);
-
+    const [visitedCountries, setVisitedCountries] = useState([]);
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
         .then(response => response.json())
         .then(data => setCountries(data))
     }, []);
+
+    const handleVisitedCountry = country => {
+        console.log('Add this to your country');
+        console.log(country);
+        setVisitedCountries(country);
+    };
     return (
         <div>
-            <h3>Countries</h3>
+            <h3>Countries: {countries.length}</h3>
+            <div>
+                <h4>Visted Countries</h4>
+                <ul>
+                    {
+                        visitedCountries.map(country => (
+                            <li key={country.cca3}>{country.name.common}</li>
+                        ))
+                    }
+                </ul>
+            </div>
             <div className="country-container">
                 {
                     countries.map(country => (
-                        <Country key={country.cca3} country={country}></Country>
+                        <Country handleVisitedCountry={handleVisitedCountry} key={country.cca3} country={country}></Country>
                     ))
                 }
             </div>
--- Module 40/react-world-tour/src/components/Country/Country.jsx
+++ Module 40/react-world-tour/src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountry}) => {
     console.log(country);
     const {name, flags, population, area, cca3} = country;
 
@@ -10,12 +10,13 @@ const Country = ({country}) => {
         setVisited(!visited);
     }
     return (
-        <div className='country'>
+        <div className={`country ${visited ? 'visited': 'non-visited'}`}>
             <h3>Name: {name?.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p><small><strong>Code: {cca3}</strong></small></p>
+            <button onClick={() => handleVisitedCountry(name.common)}>Mark Visited</button>
             <p>{visited && 'I have visited this country'}</p>
             <button onClick={handleVisited}>Visited</button>
         </div>
