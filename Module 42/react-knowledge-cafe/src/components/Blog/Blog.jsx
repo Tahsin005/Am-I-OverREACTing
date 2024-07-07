5b9d6ef36f5317df0f45af20a166a64278c41512
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-07-07 14:14:32 +0600

    blog structure

--- Module 42/react-knowledge-cafe/src/components/Blog/Blog.jsx
+++ Module 42/react-knowledge-cafe/src/components/Blog/Blog.jsx
@@ -1,9 +1,28 @@
 import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
+    const {title, cover, author_img, author, posted_date, reading_time, hashtags} = blog;
     return (
-        <div className="p-4 text-xl">
-            {blog.title}
-            <img src={blog.cover} alt="" />
+        <div className="p-4 text-xl border border-gray-700 m-4 rounded-xl">
+            
+            <img className='bg-cover' src={cover} alt={title} />
+            <div className='flex justify-between mt-4'>
+                <div className='flex'>
+                    <img className='w-14 border-8 rounded-full' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='font-semibold'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className='text-2xl font-semibold mt-4'>{title}</h2>
+            <p className='mt-4'>
+                {
+                    hashtags.map((hash, index) => <span className='text-gray-600' key={index}>#{hash}  </span>)
+                }
+            </p>
         </div>
     );
 };
--- Module 42/react-knowledge-cafe/src/components/Blogs/Blogs.jsx
+++ Module 42/react-knowledge-cafe/src/components/Blogs/Blogs.jsx
@@ -12,7 +12,7 @@ const Blogs = () => {
         .then(data => setBlogs(data))
     }, []);
     return (
-        <div className="md:w-2/3">
+        <div className="md:w-2/3 p-4">
             {
                 blogs.map(blog => (
                     <Blog key={blog.id} blog={blog}></Blog>
