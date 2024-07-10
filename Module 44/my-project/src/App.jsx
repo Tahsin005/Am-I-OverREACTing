46d37ce962d7836a417ca180496248dc4a4228bd
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-07-10 22:37:31 +0600

    Custom navbar structure

---
+++ Module 44/my-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+// import DaisyNav from "./components/DaisyNav/DaisyNav";
+
+function App() {
+  return (
+    <>
+      {/* <DaisyNav></DaisyNav> */}
+      <NavBar></NavBar>
+      <h1 className="text-4xl font-extrabold">Price Options</h1>
+    </>
+  );
+}
+
+export default App;
