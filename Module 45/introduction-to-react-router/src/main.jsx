7fe95182410d5920c2e0a913b7e2f15dff81b5b2
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-07-11 18:54:45 +0600

    bhejaiila commit

--- Module 45/introduction-to-react-router/src/main.jsx
+++ Module 45/introduction-to-react-router/src/main.jsx
@@ -1,31 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
 
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Home from './components/Home/Home.jsx';
-import Header from './components/Header/Header.jsx';
-import About from './components/About/About.jsx';
-import Contact from './components/Contact/Contact.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home></Home>,
-    children: [
-      {
-        path: "/about",
-        element: <About></About>
-      },
-      {
-        path: "/contact",
-        element: <Contact></Contact>
-      },
-    ]
+    element: <div>Hello from react rounter!!</div>,
   },
-  
+  {
+    path: "/about",
+    element: <div>I am in the about page</div>,
+  },
+  {
+    path: "/contact",
+    element: <div>I am in the contact page</div>,
+  },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
