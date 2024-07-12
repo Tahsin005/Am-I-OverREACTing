1d4b1d3035600f511c282d8ac192836de1fe565e
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-07-12 20:08:58 +0600

    active route, loading spinner, uselocation, usenavigation

--- Module 45/introduction-to-react-router-concept/src/components/Header/Header.jsx
+++ Module 45/introduction-to-react-router-concept/src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
-import { Link } from "react-router-dom";
-
+import { Link, NavLink } from "react-router-dom";
+import './Header.css'
 const Header = () => {
     return (
         <div className="flex justify-start ps-12 items-center gap-12 flex-wrap">
             <div className="text-3xl font-semibold">Nav</div>
+            
             <div className="gap-x-10 flex py-10">
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/users">Users</Link>
-                <Link to="/posts">Posts</Link>
-                <Link to="/contact">Contact</Link>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink to="/users">Users</NavLink>
+                <NavLink to="/posts">Posts</NavLink>
+                <NavLink to="/contact">Contact</NavLink>
             </div>            
         </div>
     );
--- Module 45/introduction-to-react-router-concept/src/components/Home/Home.jsx
+++ Module 45/introduction-to-react-router-concept/src/components/Home/Home.jsx
@@ -1,13 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
+    const navigation = useNavigation();
     return (
         <div>
-            <Header></Header>
+            <Header></Header> 
+            {
+                navigation.state == "loading" ?
+                <div className="text-center font-extrabold bg-lime-800 text-8xl rounded-full text-white">Loading...</div> 
+                
+                :
+                <Outlet></Outlet>
+            }
             <h1 className="text-7xl text-center px-12">Welcome to my website</h1>
-            <Outlet></Outlet>
+            
             <Footer className=""></Footer>
         </div>
     );
