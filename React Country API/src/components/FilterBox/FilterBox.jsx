c2ec3c861e1083a5518f9759a59a24a4176b9ff1
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-07-13 12:16:00 +0600

    filter by region

--- React Country API/src/components/FilterBox/FilterBox.jsx
+++ React Country API/src/components/FilterBox/FilterBox.jsx
@@ -1,12 +1,13 @@
-const FilterBox = () => {
+const FilterBox = ({ setQuery }) => {
   return (
-    <div className="w-64">
-      <select>
-        <option>Filter By Region</option>
-        <option>Material Tailwind React</option>
-        <option>Material Tailwind Vue</option>
-        <option>Material Tailwind Angular</option>
-        <option>Material Tailwind Svelte</option>
+    <div className="w-64 border-2 rounded-lg">
+      <select className="mt-3" onChange={(e) => setQuery(e.target.value.toLocaleLowerCase())}>
+        <option hidden >Filter by Region</option>
+        <option value="Africa">Africa</option>
+        <option value="Americas">Americas</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="Oceania">Oceania</option>
       </select>
     </div>
   );
