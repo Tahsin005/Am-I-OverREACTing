ef29b4cb016fd575f9441f433f71f44b1ad0ad3d
tahsin005 <tahsin.ferdous3546@gmail.com>
2024-07-14 11:12:42 +0600

    form validation

--- Module 46/form-master/src/components/StatefulForm/StatefulForm.jsx
+++ Module 46/form-master/src/components/StatefulForm/StatefulForm.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 
 const StatefulForm = () => {
-    const [name, setName] = useState(null);
+    const [name, setName] = useState('Rojoni Klanto');
     const [email, setEmail] = useState(null);
     const [password, setPassword] = useState(null);
+    const [error, setError] = useState('');
     const handleSubmit = e => {
         e.preventDefault();
-        console.log(name, email, password);
+        if (password.length < 6) {
+            setError('password must be at least 6 characters')
+        } else {
+            console.log(name, email, password);
+        }
     }
 
     const handleNameChange = e => {
@@ -26,13 +31,16 @@ const StatefulForm = () => {
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input onChange={handleNameChange} type="text" name="name" />
+                <input value={name} onChange={handleNameChange} type="text" name="name" />
                 <br />
                 <input onChange={handleEmailChange} type="email" name="email" />
                 <br />
                 <input onChange={handlePasswordChange} type="text" name="phone" />
                 <br />
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Submit" required />
+                {
+                    error && <p>{error}</p>
+                }
             </form>     
         </div>
     );
